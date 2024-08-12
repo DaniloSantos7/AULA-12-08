0e80f6a799360d2bfaaa802d3e65dee6c2aef413
DaniloSantos7 <159054755+DaniloSantos7@users.noreply.github.com>
2024-08-12 13:54:20 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,23 @@
+-- TABLE
+CREATE TABLE campo(
+  		ID INT(4),
+  		CIDADE VARCHAR(30) UNIQUE,
+  		CAPCACIDADE INT (4) NOT NULL
+  
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE jogador(
+    ID INT(4),
+  NOME VARCHAR(30),
+  ESPORTE VARCHAR (30)
+  
+  	
+
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
